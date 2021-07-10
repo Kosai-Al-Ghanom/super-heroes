bb8c676308d759157e849b52f601ff9ca926ecad
Kosai-Al-Ghanom <kosai.alghanom@powercoders.org>
2021-07-10 20:42:37 +0200

    search heroes

--- super-heroes/src/app/heroes/heroes.component.ts
+++ super-heroes/src/app/heroes/heroes.component.ts
@@ -13,6 +13,8 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
   heroes: Hero[] = [];
+  name: string = "";
+
 
   constructor(private heroservice: HeroService,) { }
 
@@ -28,4 +30,15 @@ export class HeroesComponent implements OnInit {
     this.heroservice.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
+
+  Search(): void{
+    if(this.name != ""){
+    this.heroes = this.heroes.filter(res=>{
+      return res.name.toLocaleLowerCase().match(this.name.trim())
+    })
+  } else if(this.name == ""){
+    this.ngOnInit();
+  }
+  }
+  
 }
